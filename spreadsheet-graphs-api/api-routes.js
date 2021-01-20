1d5cc491844ed5a4a68d14de80fc7d96eda952ee
Jimmy Jiang <james.jiang30@gmail.com>
2021-01-19 19:32:28 -0700

    API-general-template

--- spreadsheet-graphs-api/api-routes.js
+++ spreadsheet-graphs-api/api-routes.js
@@ -9,5 +9,21 @@ router.get("/", function (req, res) {
   });
 });
 
+// Import data point controller
+var dataPointController = require("./dataController");
+
+// data point routes
+router
+  .route("/data-points")
+  .get(dataPointController.index)
+  .post(dataPointController.new);
+
+router
+  .route("/data-points/:dataPoint_id")
+  .get(dataPointController.view)
+  .patch(dataPointController.update)
+  .put(dataPointController.update)
+  .delete(dataPointController.delete);
+
 // Export API routes
 module.exports = router;
---
+++ spreadsheet-graphs-api/dataController.js
@@ -0,0 +1,85 @@
+// dataController.js
+
+// Import data model
+DataPoint = require("./dataPointModel");
+// Handle index actions
+exports.index = function (req, res) {
+  DataPoint.get(function (err, dataPoints) {
+    if (err) {
+      res.json({
+        status: "error",
+        message: err,
+      });
+    }
+    res.json({
+      status: "success",
+      message: "Data retrieved successfully",
+      data: dataPoints,
+    });
+  });
+};
+// Handle create data point actions
+exports.new = function (req, res) {
+  var dataPoint = new DataPoint();
+  dataPoint.name = req.body.name ? req.body.name : dataPoint.name;
+  dataPoint.gender = req.body.gender;
+  dataPoint.email = req.body.email;
+  dataPoint.phone = req.body.phone;
+
+  // save the data point and check for errors
+  dataPoint.save(function (err) {
+    // if (err)
+    //     res.json(err);
+    res.json({
+      message: "New data point created!",
+      data: dataPoint,
+    });
+  });
+};
+// Handle view data point info
+exports.view = function (req, res) {
+  DataPoint.findById(req.params.dataPoint_id, function (err, dataPoint) {
+    if (err) res.send(err);
+    res.json({
+      message: "data point details loading..",
+      data: dataPoint,
+    });
+  });
+};
+// Handle update data point info
+exports.update = function (req, res) {
+  DataPoint.findById(req.params.dataPoint_id, function (err, dataPoint) {
+    if (err) res.send(err);
+
+    dataPoint.name = req.body.name ? req.body.name : dataPoint.name;
+    dataPoint.gender = req.body.gender;
+    dataPoint.email = req.body.email;
+    dataPoint.phone = req.body.phone;
+
+    // save the data point and check for errors
+    dataPoint.save(function (err) {
+      if (err) res.json(err);
+      res.json({
+        message: "data point Info updated",
+        data: dataPoint,
+      });
+    });
+  });
+};
+
+// Handle delete data point
+exports.delete = function (req, res) {
+  DataPoint.remove(
+    {
+      _id: req.params.dataPoint_id,
+    },
+    function (err, dataPoint) {
+      if (err) res.send(err);
+
+      res.json({
+        status: "success",
+        message: "data point deleted",
+      });
+    }
+  );
+};
