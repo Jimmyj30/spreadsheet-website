776f873ec6a013b283656d8dd9d37228df5f54a1
Jimmy Jiang <james.jiang30@gmail.com>
2021-04-06 20:54:31 -0600

    updated data table data model

--- spreadsheet-graphs-api/dataTableModel.js
+++ spreadsheet-graphs-api/dataTableModel.js
@@ -2,6 +2,7 @@
 var mongoose = require("mongoose");
 // https://mongoosejs.com/docs/schematypes.html#arrays
 
+// curve straightening instructions...
 const Instruction = new mongoose.Schema({
   //"type" of function -- eg: LOG_BASE_E, LOG_BASE_10, TO_CONST_POWER, MULTIPLIED_BY_CONSTANT_VARIABLE...
   functionClass: {
@@ -19,28 +20,34 @@ const Instruction = new mongoose.Schema({
   },
 });
 
+// data points (one "row" of a data table)...
+const dataPoint = new mongoose.Schema({
+  xCoord: {
+    type: Number,
+    required: true,
+  },
+  yCoord: {
+    type: Number,
+    required: true,
+  },
+  xUncertainty: {
+    type: Number,
+    required: true,
+  },
+  yUncertainty: {
+    type: Number,
+    required: true,
+  },
+});
+
 module.exports.Instruction = Instruction;
+module.exports.dataPoint = dataPoint;
 
 // Setup schema
 var dataTableSchema = mongoose.Schema({
-  xCoords: {
-    // “x” coordinates of the data table
-    type: Array,
-    required: true,
-  },
-  yCoords: {
-    // “y” coordinates of the data table
-    type: Array,
-    required: true,
-  },
-  xUncertainties: {
-    // uncertainties corresponding to each “x” coordinate
-    type: Array,
-    required: true,
-  },
-  yUncertainties: {
-    // uncertainties corresponding to each “y” coordinate
-    type: Array,
+  dataTableData: {
+    // the numeric data stored in the data table
+    type: [dataPoint],
     required: true,
   },
   xLabel: {
@@ -63,6 +70,28 @@ var dataTableSchema = mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+
+  // ******* old data format for the table *************
+  // xCoords: {
+  //   // “x” coordinates of the data table
+  //   type: Array,
+  //   required: true,
+  // },
+  // yCoords: {
+  //   // “y” coordinates of the data table
+  //   type: Array,
+  //   required: true,
+  // },
+  // xUncertainties: {
+  //   // uncertainties corresponding to each “x” coordinate
+  //   type: Array,
+  //   required: true,
+  // },
+  // yUncertainties: {
+  //   // uncertainties corresponding to each “y” coordinate
+  //   type: Array,
+  //   required: true,
+  // },
 });
 
 // Export data table model
