bce8367c5791d9dc80db0fbe388756a2d84e4d09
Jimmy Jiang <james.jiang30@gmail.com>
2021-04-30 21:35:49 -0600

    heroku deployment tutorial url

--- spreadsheet-graphs-api/index.js
+++ spreadsheet-graphs-api/index.js
@@ -2,8 +2,6 @@
 const cors = require("cors");
 // Import express
 let express = require("express");
-// Import Body parser
-let bodyParser = require("body-parser");
 // Import Mongoose
 let mongoose = require("mongoose");
 // Initialize the app
@@ -12,14 +10,17 @@ app.use(cors());
 
 // Import routes
 let apiRoutes = require("./api-routes");
-// Configure bodyparser to handle post requests
+
+// Deploy on Heroku: https://developer.mongodb.com/how-to/use-atlas-on-heroku/
+
+// Body parser now depreciated: use "express" to handle requests instead
+app.use(express.json());
 app.use(
-  bodyParser.urlencoded({
+  express.urlencoded({
     extended: true,
   })
 );
 
-app.use(bodyParser.json());
 // Connect to Mongoose and set connection variable
 mongoose.connect("mongodb://localhost/spreadsheet-graphs-api", {
   useNewUrlParser: true,
