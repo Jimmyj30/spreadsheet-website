38a9555b7388e11115ca7f3d8dda2a52b7416035
Jimmy Jiang <james.jiang30@gmail.com>
2021-10-01 00:31:11 -0600

    added help page and navbar

--- spreadsheet-graphs-website/src/app/app.module.ts
+++ spreadsheet-graphs-website/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
 import { HotTableModule } from '@handsontable/angular';
 import * as PlotlyJS from 'plotly.js/dist/plotly.js';
 import { PlotlyModule } from 'angular-plotly.js';
@@ -11,11 +12,25 @@ import { PlotlyModule } from 'angular-plotly.js';
 import { AppComponent } from './app.component';
 import { DataTablesComponent } from './data-tables/data-tables.component';
 import { GraphsComponent } from './data-tables/graphs/graphs.component';
+import { HeaderComponent } from './header/header.component';
+import { HelpComponent } from './help/help.component';
+
+const appRoutes: Routes = [
+  { path: '', component: DataTablesComponent },
+  { path: 'help', component: HelpComponent },
+  { path: '**', component: DataTablesComponent },
+];
 
 // adding the plotly.js module to the Angular library
 PlotlyModule.plotlyjs = PlotlyJS;
 @NgModule({
-  declarations: [AppComponent, DataTablesComponent, GraphsComponent],
+  declarations: [
+    AppComponent,
+    DataTablesComponent,
+    GraphsComponent,
+    HeaderComponent,
+    HelpComponent,
+  ],
   imports: [
     BrowserModule,
     HotTableModule.forRoot(),
@@ -25,6 +40,7 @@ PlotlyModule.plotlyjs = PlotlyJS;
     PlotlyModule,
     MatProgressSpinnerModule,
     BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [HttpClientModule],
   bootstrap: [AppComponent],
