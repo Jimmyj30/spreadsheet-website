e62e886c3d3899a3d643ff927fefae336f100db1
Jimmy Jiang <james.jiang30@gmail.com>
2021-12-19 03:06:10 -0700

    initial forms component

--- spreadsheet-graphs-website/src/app/data-tables/data-tables.component.ts
+++ spreadsheet-graphs-website/src/app/data-tables/data-tables.component.ts
@@ -1,6 +1,11 @@
 import { ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
 import { HotTableComponent, HotTableRegisterer } from '@handsontable/angular';
-import { FormControl, FormBuilder, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormBuilder,
+  Validators,
+  FormGroup,
+} from '@angular/forms';
 import * as Handsontable from 'handsontable';
 
 import { DataTableService } from '../shared/data-table.service';
@@ -16,21 +21,6 @@ const FILL_OUT_SPREADSHEET_FULLY_MESSAGE =
   styleUrls: ['./data-tables.component.css'],
 })
 export class DataTablesComponent implements OnInit {
-  // https://handsontable.com/docs/8.3.2/tutorial-data-sources.html
-  // (Object data source with custom data schema section)
-
-  // https://stackoverflow.com/questions/46007985/refresh-handsontable-angular-4
-  // use @ViewChild and refresh the new handsontable....
-
-  // https://www.positronx.io/angular-7-select-dropdown-examples-with-reactive-forms/
-  // https://angular.io/guide/form-validation#defining-custom-validators
-
-  // https://developer.okta.com/blog/2019/10/03/painless-node-authentication
-  // https://medium.com/swlh/set-up-an-express-js-app-with-passport-js-and-mongodb-for-password-authentication-6ea05d95335c
-
-  // to run locally: ng serve --proxy-config proxy.conf.json
-  // https://medium.com/@ryanchenkie_40935/angular-cli-deployment-host-your-angular-2-app-on-heroku-3f266f13f352
-
   private hotRegisterer = new HotTableRegisterer();
 
   rawData: any[];
@@ -45,10 +35,10 @@ export class DataTablesComponent implements OnInit {
 
   showXToConstantPower: boolean;
   showYToConstantPower: boolean;
-  xOptions: any = ['x', 'ln(x)', 'log_10(x)', 'x^a'];
+  xOptions: any = ['x', 'ln(x)', 'log_10(x)', 'x^a']; // move things like this to a constants file
   yOptions: any = ['y', 'ln(y)', 'log_10(y)', 'y^a'];
 
-  curveStraighteningInstructionsForm = this.fb.group({
+  curveStraighteningInstructionsForm: FormGroup = this.fb.group({
     xCurveStraighteningInstructions: new FormControl(this.xOptions[0], []),
     xToConstantPower: new FormControl('', []),
     yCurveStraighteningInstructions: new FormControl(this.yOptions[0], []),
@@ -85,7 +75,7 @@ export class DataTablesComponent implements OnInit {
         'Responding Variable',
         'Uncertainties for<br>Responding Variable',
       ],
-      columns: dataTableService.dataTableDefaultColumnValues,
+      columns: this.dataTableService.dataTableDefaultColumnValues,
 
       afterChange: (changes) => {
         this.validateHandsontable();
@@ -185,50 +175,6 @@ export class DataTablesComponent implements OnInit {
     );
   }
 
-  changeXOption(event) {
-    if (this.removeFirstWord(event.target.value) === 'x^a') {
-      // event.target.value will either equal "3: x^a" or "x^a"
-      this.showXToConstantPower = true;
-      this.curveStraighteningInstructionsForm.controls.xToConstantPower.setValidators(
-        [Validators.required, numberFractionValidator()]
-      );
-      this.curveStraighteningInstructionsForm.controls.xToConstantPower.updateValueAndValidity();
-    } else {
-      this.showXToConstantPower = false;
-      this.curveStraighteningInstructionsForm.patchValue({
-        xToConstantPower: undefined,
-      });
-      this.curveStraighteningInstructionsForm.controls.xToConstantPower.clearValidators();
-      this.curveStraighteningInstructionsForm.controls.xToConstantPower.updateValueAndValidity();
-    }
-
-    this.xCurveStraighteningInstructions.setValue(event.target.value, {
-      onlySelf: true,
-    });
-  }
-
-  changeYOption(event) {
-    if (this.removeFirstWord(event.target.value) === 'y^a') {
-      // event.target.value will either equal "3: y^a" or "y^a"
-      this.showYToConstantPower = true;
-      this.curveStraighteningInstructionsForm.controls.yToConstantPower.setValidators(
-        [Validators.required, numberFractionValidator()]
-      );
-      this.curveStraighteningInstructionsForm.controls.yToConstantPower.updateValueAndValidity();
-    } else {
-      this.showYToConstantPower = false;
-      this.curveStraighteningInstructionsForm.patchValue({
-        yToConstantPower: undefined,
-      });
-      this.curveStraighteningInstructionsForm.controls.yToConstantPower.clearValidators();
-      this.curveStraighteningInstructionsForm.controls.yToConstantPower.updateValueAndValidity();
-    }
-
-    this.yCurveStraighteningInstructions.setValue(event.target.value, {
-      onlySelf: true,
-    });
-  }
-
   onSubmit(): void {
     // form instructions...
     this.rawDataTable = this.dataTableService.generateRawDataTable(
@@ -239,11 +185,10 @@ export class DataTablesComponent implements OnInit {
     // console.log('rawDataTable: ');
     // console.log(this.rawDataTable);
 
-    // we send the raw data table using the API, and the API will store that raw data table (giving it an ID), and
-    // will also send a processed data table in the response as well...
+    // we send the raw data table using the API, and the API will store that raw data table (giving it an ID)
     // response might contain a message, a processedDataTable, and a rawDataTable id (_id) ...
     // we could then display the processedDataTable and give the rawDataTable an id
-    // that will need to be used if we want update the processed data table...
+    // that will need to be used if we want update the raw data table...
 
     // if the processedDataTable exists and rawDataTable has an ID then
     // rawDataTable has been saved to the database
@@ -252,8 +197,6 @@ export class DataTablesComponent implements OnInit {
       this.loading = true;
       this.dataTableService.updateDataTable(this.rawDataTable).subscribe(
         (response: any) => {
-          // console.log('update table response: ');
-          // console.log(response);
           this.loading = false;
           this.updateProcessedDataTableSettings(response.data);
           this.error = undefined;
@@ -267,22 +210,14 @@ export class DataTablesComponent implements OnInit {
       this.loading = true;
       this.dataTableService.createDataTable(this.rawDataTable).subscribe(
         (response: any) => {
-          // console.log('create table response: ');
-          // console.log(response);
-
-          // add an ID to the raw data table to indicate it is now stored in the database
-          // return a processed data table as the response....
-          // response contains a processedDataTable and a rawDataTableID
-
-          // this.processedDataTable is a plain object since only the raw data
-          // table data can be changed by the user — the raw data gets updated in the
-          // DB every time that it gets submitted to the API (creating/updating the
-          // raw data table) and the processedDataTable is part of the API response
+          // response contains a processedDataTable and a rawDataTableID...
+          // this.processedDataTable is a plain object since the raw data table the user submits
+          // can be used to calculate a processedDataTable that can then be sent as part of a request response
           this.loading = false;
           this.processedDataTable = response.processedDataTable;
           this.createProcessedDataTableSettings(response.processedDataTable);
 
-          //(gives raw data table an ID)
+          // add an ID to the raw data table to indicate it is now stored in the database
           this.rawDataTable._id = response.rawDataTableID;
           this.error = undefined;
         },
@@ -375,8 +310,6 @@ export class DataTablesComponent implements OnInit {
     this.changeDetector.detectChanges();
     this.processedDataTableSettings.data = dataTable.dataTableData;
     this.refreshProcessedDataTable(this.processedDataTableSettings);
-    // changing the data of the processed data table based on the response...
-
     // console.log('updated data table: ');
     // console.log(this.processedDataTable);
   }
@@ -389,13 +322,8 @@ export class DataTablesComponent implements OnInit {
     this.rawDataTableRef.updateHotTable(settings);
   }
 
-  private removeFirstWord(string: string): string {
-    return string.substring(string.indexOf(' ') + 1);
-  }
-
-  // you can shift the decimal place left as many
-  // times as you want so there is no validity check
-  // for this operation
+  // you can shift the decimal place left as many times as you want
+  // so there is no validity check for this operation
   private shiftDecimalPlaceLeft(columnIndex, dataTable) {
     let dataTableVar;
     if (dataTable === 'rawDataTable') {
@@ -472,7 +400,7 @@ export class DataTablesComponent implements OnInit {
     ) {
       this.invalidFormErrorMsg = undefined;
     } else {
-      // default value for table without anything filled in yet
+      // default error message for table without anything filled in yet
       this.invalidFormErrorMsg = FILL_OUT_SPREADSHEET_FULLY_MESSAGE;
     }
   }
